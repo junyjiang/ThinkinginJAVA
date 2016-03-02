c4cb8b03ab318f25b834b0672873336fa00f88f0
jiangchao3 <jiangchao3@lenovo.com>
2016-03-02 16:56:04 +0800

    update Fifth_01

---
+++ src/Fifth/Fifth_01.java
@@ -0,0 +1,16 @@
+package Fifth;
+
+public class Fifth_01 {
+	public static void main(String[] args){
+		String s="dd";
+//		Test tt = new Test();
+//		tt.Test(s);
+		new Test(s);
+	}
+	public static class Test{	
+		public Test(String str) {
+			// TODO Auto-generated constructor stub
+			System.out.println(str);
+		}
+	}
+}
